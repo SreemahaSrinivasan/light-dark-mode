25e2f090690193f0fd917220c4577cf8a64dd766
SreemahaSrinivasan <srimahas2001@gmail.com>
2025-03-24 07:47:03 +0000

    updated code

--- src/components/LightDarkMode/index.js
+++ src/components/LightDarkMode/index.js
@@ -1,1 +1,44 @@
 // Write your code here
+import './index.css'
+import {Component} from 'react'
+
+class LightDarkMode extends Component {
+  state = {isDarkMode: true}
+
+  onChangeButtonStatus = () => {
+    this.setState(prevState => ({
+      isDarkMode: !prevState.isDarkMode,
+    }))
+  }
+
+  onChangeButtonText = () => {
+    const {isDarkMode} = this.state
+    return isDarkMode ? 'Light Mode' : 'Dark Mode'
+  }
+
+  onChangeClassName = () => {
+    const {isDarkMode} = this.state
+    return isDarkMode ? 'dark-mode' : 'light-mode'
+  }
+
+  render() {
+    const buttonText = this.onChangeButtonText()
+    const containerClassName = this.onChangeClassName()
+    return (
+      <div className="app-container">
+        <div className={`container ${containerClassName}`}>
+          <h1 className="heading">Click To Change Mode</h1>
+          <button
+            type="button"
+            className="button"
+            onClick={this.onChangeButtonStatus}
+          >
+            {buttonText}
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default LightDarkMode
